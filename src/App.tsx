b70f7d244b6c3a0ff4d693d2261117d30b149cef
rushi <rushi44@gmail.com>
2021-04-18 22:13:43 -0700

    finished demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,57 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-balham.css";
+
+export const App = () => {
+  const [state, setState] = useState<any>({
+    columnDefs: [
+      {
+        headerName: "Make",
+        field: "make",
+        sortable: true,
+        filter: true,
+        checkboxSelection: true,
+      },
+      { headerName: "Model", field: "model", sortable: true, filter: true },
+      { headerName: "Price", field: "price", sortable: true, filter: true },
+    ],
+    rowData: [],
+  });
+
+  const [gridApi, setGridApi] = useState<any>();
+
+  const fetchCarData = async () =>
+    void setState({
+      ...state,
+      rowData: [
+        { make: "Toyota", model: "Celica", price: 35000 },
+        { make: "Ford", model: "Fiesta", price: 20000 },
+        { make: "Porche", model: "GT3", price: 100000 },
+      ],
+    });
+
+  const selectedRowsAlert = () => {
+    const selectedNodes = gridApi.getSelectedNodes();
+    const selectedData = selectedNodes.map((node) => node.data);
+    const selectedDataStringPresentation = selectedData
+      .map((node) => `${node.make} ${node.model}`)
+      .join(", ");
+    alert(`Selected Nodes: ${selectedDataStringPresentation}`);
+  };
+
+  useEffect(() => void fetchCarData(), []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="ag-theme-balham" style={{ width: 500, height: 600 }}>
+      <AgGridReact
+        columnDefs={state.columnDefs}
+        rowData={state.rowData}
+        rowSelection="multiple"
+        onGridReady={(params) => setGridApi(params.api)}
+      />
+      <button onClick={selectedRowsAlert}>Get selected rows</button>
     </div>
-  )
-}
-
-export default App
+  );
+};
